39b6f35589c61b12ffa3c73be130dd23cbc49a46
Maria Saavedra <msaav3@gmail.com>
2024-02-04 22:06:16 -0600

    chore: added table

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,23 @@
+import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 import AppTable from "./components/app-table";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [foundApps, setFoundApps] = useState<Array<AppInfo>>();
+
+  async function searchApps() {
+    setFoundApps(await invoke("search_apps", { term: "" }));
+  }
+
+  useEffect(() => {
+    searchApps();
+  }, []);
+
   return (
     <section>
-      <AppTable />
+      <h1>Tauri-Zen</h1>
+      <AppTable data={foundApps || []} />
     </section>
   );
 }
